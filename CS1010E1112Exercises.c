c453b136f78a0c0e3022d2305abed167dfe8da1b
code_tofu <benjamin.ftc@gmail.com>
2022-06-14 08:47:07 +0800

    Removed Coursera W5 Exercise. Included CS1010E1112

---
+++ CS1010E1112Exercises.c
@@ -0,0 +1,7 @@
+/*
+CS1010E 1112L2P1: What time is it?
+Problem statement:
+Given a start time ST and end time ET, expressed by the hour (0 to 23) and minute (0 to 59)
+Show the total elapsed time (hour and minute) between start time and end time 
+For simplicity: You can assume that ST and ET are in the same day, so ST is always smaller than ET
+*/
