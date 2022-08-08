a55dadc8bc13740f9644a0231c2f115da46042b2
code-tofu <89731295+code-tofu@users.noreply.github.com>
2022-08-08 17:05:05 +0800

    update L4P2.c

--- L4P2.c
+++ L4P2.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+float circlearea(float radius);
 
 int main(){
-    void circlearea(float radius);
+    
+    float area = 0, R1 = 0, R2 = 0, R3=0, 
+    area = 
 }
 
-void circlearea(float radius)
+float circlearea(float radius){
+    float area=0
+    area = radius * radius * 3.14159;
+    return area;
+}
