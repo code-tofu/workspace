6773a7774bcb582e323b3c8a01fa8e240742f6ed
code-tofu <benjamin.ftc@gmail.com>
2022-12-18 01:22:12 +0800

    initialise workspace

--- C-NOTES_UDEMY.c
+++
@@ -1,7 +0,0 @@
-
-/*
-Section 12 - Pointers
-variable that stores and address. that address points to another lcoation in memory that contains a value
-e.g. scanf() reads into a specified address
-dereferencing a pointer obtains the value at the memory location where the pointer points to
-/
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # WORKSPACE:
 
+Practice Recursion for:
+- Mario
+- Factorial
+- Fibonacci
+- Collatz Conjecture
+
+-Merge Sort
+-Selection Sort
+-Insertion Sort
+-Bubble Sort
+-Binary Searc
+-Linear Search
