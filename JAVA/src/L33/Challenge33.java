a797d2599e8cb1772ded081845af79619d7572cc
code-tofu <benjamin.ftc@gmail.com>
2023-01-31 19:34:51 +0800

    plan updated

--- JAVA/src/L33/Challenge33.java
+++ JAVA/src/L33/Challenge33.java
@@ -4,5 +4,11 @@ public class Hello {
         System.out.print("Hello, User!");
         System.out.println("Hello again!");
         //println will add a new line after output
+
+        boolean isAlien = false;
+        if (isAlien == false) { //use !isAlien
+            //note no semicolon, use code block
+            System.out.println("You are from this world!");
+        }
     }
 }
