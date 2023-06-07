7069f09a6233a59784c2ee48a47ae2bda8b22678
code-tofu <benjamin.ftc@gmail.com>
2023-06-07 16:08:23 +0800

    Moved Pokemart out

--- PokeMartBeta/_queries/db.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE IF EXISTS pokemart;
-CREATE DATABASE pokemart;
-CREATE user 'testoak'@'localhost' identified by 'passoak5%';
-GRANT ALL PRIVILEGES ON pokemart.* TO 'testoak'@'localhost';
-
-SHOW DATABASES;
-USE pokemart;
-SHOW TABLES;
